8629da8a91dd72d49a0b10549963d35632fdfbae
masterprog-cmd <xaviabadgome01@gmail.com>
2021-11-16 12:04:25 +0100

    hide check mode banner

--- practicas/lib/main.dart
+++ practicas/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Componentes'),
