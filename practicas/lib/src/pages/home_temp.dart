6e35700150406ea4af16a15c36e431ede5c169f5
masterprog-cmd <xaviabadgome01@gmail.com>
2021-11-29 16:10:10 +0100

    Ex completed

--- practicas/lib/src/pages/home_temp.dart
+++ practicas/lib/src/pages/home_temp.dart
@@ -1,154 +1,147 @@
 import 'package:flutter/material.dart';
-import 'package:practicas/src/providers/home_alerts.dart';
 
-class HomeTemp extends StatelessWidget {
-  final opciones = ["Primero", "Segundo", "Tercero"];
+class HomeTemp extends StatefulWidget {
   HomeTemp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.all(10),
-      children: _crearItemCorto(),
-      // children: <Widget>[
-      //   ListTile(
-      //     leading: const Icon(
-      //       Icons.notifications_outlined,
-      //       color: Colors.blue,
-      //     ),
-      //     title: const Text("Alertas"),
-      //     onTap: () => Navigator.push(
-      //       context,
-      //       MaterialPageRoute(builder: (context) => HomeAlerts()),
-      //     ),
-      //     trailing: Row(
-      //       mainAxisSize: MainAxisSize.min,
-      //       children: const [
-      //         Icon(
-      //           Icons.navigate_next_outlined,
-      //           color: Colors.blue,
-      //         )
-      //       ],
-      //     ),
-      //   ),
-      //   ListTile(
-      //     leading: const Icon(
-      //       Icons.accessibility_outlined,
-      //       color: Colors.blue,
-      //     ),
-      //     title: const Text("Avatar"),
-      //     trailing: Row(
-      //       mainAxisSize: MainAxisSize.min,
-      //       children: const [
-      //         Icon(
-      //           Icons.navigate_next_outlined,
-      //           color: Colors.blue,
-      //         )
-      //       ],
-      //     ),
-      //   ),
-      //   ListTile(
-      //     leading: const Icon(
-      //       Icons.folder_outlined,
-      //       color: Colors.blue,
-      //     ),
-      //     title: const Text("Cards-Tarjetas"),
-      //     trailing: Row(
-      //       mainAxisSize: MainAxisSize.min,
-      //       children: const [
-      //         Icon(
-      //           Icons.navigate_next_outlined,
-      //           color: Colors.blue,
-      //         )
-      //       ],
-      //     ),
-      //   ),
-      //   ListTile(
-      //     leading: const Icon(
-      //       Icons.donut_large_outlined,
-      //       color: Colors.blue,
-      //     ),
-      //     title: const Text("Animated Container"),
-      //     trailing: Row(
-      //       mainAxisSize: MainAxisSize.min,
-      //       children: const [
-      //         Icon(
-      //           Icons.navigate_next_outlined,
-      //           color: Colors.blue,
-      //         )
-      //       ],
-      //     ),
-      //   ),
-      //   ListTile(
-      //     leading: const Icon(
-      //       Icons.input_outlined,
-      //       color: Colors.blue,
-      //     ),
-      //     title: const Text("Inputs"),
-      //     trailing: Row(
-      //       mainAxisSize: MainAxisSize.min,
-      //       children: const [
-      //         Icon(
-      //           Icons.navigate_next_outlined,
-      //           color: Colors.blue,
-      //         )
-      //       ],
-      //     ),
-      //   ),
-      //   ListTile(
-      //     leading: const Icon(
-      //       Icons.tune_outlined,
-      //       color: Colors.blue,
-      //     ),
-      //     title: const Text("Slider-Checks"),
-      //     trailing: Row(
-      //       mainAxisSize: MainAxisSize.min,
-      //       children: const [
-      //         Icon(
-      //           Icons.navigate_next_outlined,
-      //           color: Colors.blue,
-      //         )
-      //       ],
-      //     ),
-      //   ),
-      //   ListTile(
-      //     leading: const Icon(
-      //       Icons.format_list_bulleted_outlined,
-      //       color: Colors.blue,
-      //     ),
-      //     title: const Text("Listas y Scroll"),
-      //     trailing: Row(
-      //       mainAxisSize: MainAxisSize.min,
-      //       children: const [
-      //         Icon(
-      //           Icons.navigate_next_outlined,
-      //           color: Colors.blue,
-      //         )
-      //       ],
-      //     ),
-      //   ),
-      // ],
-    );
-  }
+  State<HomeTemp> createState() => _HomeTempState();
+}
 
-  List<Widget> _crearitems() {
-    final lista = <Widget>[];
-    opciones.forEach(
-      (element) {
-        final tempWidget = ListTile(
-          title: Text(element),
-        );
-        lista.add(tempWidget);
-        lista.add(
-          const Divider(
-            indent: 60,
-            endIndent: 60,
-            color: Colors.blue,
+class _HomeTempState extends State<HomeTemp> {
+  final opciones = ["Primero", "Segundo", "Tercero"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: _crearItemCorto(),
+            ),
           ),
-        );
-      },
+          button(),
+          // children: <Widget>[
+          //   ListTile(
+          //     leading: const Icon(
+          //       Icons.notifications_outlined,
+          //       color: Colors.blue,
+          //     ),
+          //     title: const Text("Alertas"),
+          //     onTap: () => Navigator.push(
+          //       context,
+          //       MaterialPageRoute(builder: (context) => HomeAlerts()),
+          //     ),
+          //     trailing: Row(
+          //       mainAxisSize: MainAxisSize.min,
+          //       children: const [
+          //         Icon(
+          //           Icons.navigate_next_outlined,
+          //           color: Colors.blue,
+          //         )
+          //       ],
+          //     ),
+          //   ),
+          //   ListTile(
+          //     leading: const Icon(
+          //       Icons.accessibility_outlined,
+          //       color: Colors.blue,
+          //     ),
+          //     title: const Text("Avatar"),
+          //     trailing: Row(
+          //       mainAxisSize: MainAxisSize.min,
+          //       children: const [
+          //         Icon(
+          //           Icons.navigate_next_outlined,
+          //           color: Colors.blue,
+          //         )
+          //       ],
+          //     ),
+          //   ),
+          //   ListTile(
+          //     leading: const Icon(
+          //       Icons.folder_outlined,
+          //       color: Colors.blue,
+          //     ),
+          //     title: const Text("Cards-Tarjetas"),
+          //     trailing: Row(
+          //       mainAxisSize: MainAxisSize.min,
+          //       children: const [
+          //         Icon(
+          //           Icons.navigate_next_outlined,
+          //           color: Colors.blue,
+          //         )
+          //       ],
+          //     ),
+          //   ),
+          //   ListTile(
+          //     leading: const Icon(
+          //       Icons.donut_large_outlined,
+          //       color: Colors.blue,
+          //     ),
+          //     title: const Text("Animated Container"),
+          //     trailing: Row(
+          //       mainAxisSize: MainAxisSize.min,
+          //       children: const [
+          //         Icon(
+          //           Icons.navigate_next_outlined,
+          //           color: Colors.blue,
+          //         )
+          //       ],
+          //     ),
+          //   ),
+          //   ListTile(
+          //     leading: const Icon(
+          //       Icons.input_outlined,
+          //       color: Colors.blue,
+          //     ),
+          //     title: const Text("Inputs"),
+          //     trailing: Row(
+          //       mainAxisSize: MainAxisSize.min,
+          //       children: const [
+          //         Icon(
+          //           Icons.navigate_next_outlined,
+          //           color: Colors.blue,
+          //         )
+          //       ],
+          //     ),
+          //   ),
+          //   ListTile(
+          //     leading: const Icon(
+          //       Icons.tune_outlined,
+          //       color: Colors.blue,
+          //     ),
+          //     title: const Text("Slider-Checks"),
+          //     trailing: Row(
+          //       mainAxisSize: MainAxisSize.min,
+          //       children: const [
+          //         Icon(
+          //           Icons.navigate_next_outlined,
+          //           color: Colors.blue,
+          //         )
+          //       ],
+          //     ),
+          //   ),
+          //   ListTile(
+          //     leading: const Icon(
+          //       Icons.format_list_bulleted_outlined,
+          //       color: Colors.blue,
+          //     ),
+          //     title: const Text("Listas y Scroll"),
+          //     trailing: Row(
+          //       mainAxisSize: MainAxisSize.min,
+          //       children: const [
+          //         Icon(
+          //           Icons.navigate_next_outlined,
+          //           color: Colors.blue,
+          //         )
+          //       ],
+          //     ),
+          //   ),
+          // ],
+        ],
+      ),
     );
-    return lista;
   }
 
   List<Widget> _crearItemCorto() {
@@ -174,6 +167,19 @@ class HomeTemp extends StatelessWidget {
           ),
         )
         .toList();
+
     return widget;
+  }
+
+  Widget button() {
+    return FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(
+            () {
+              opciones.add("New ListTitle");
+            },
+          );
+        });
   }
 }
